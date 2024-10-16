af88d3dc6c813389cb433c348e134c3788eccd2a
Jeriel <jerielstarosky@hotmail.com>
2024-10-16 19:02:10 -0300

    commit inicial

---
+++ src/test/java/br/com/msj/gestao/GestaoContabilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msj.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoContabilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
